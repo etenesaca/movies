8c7ec3936de5165f9eda7bae16c734a9037cc8a8
etenesaca <edgartenesaca@gmail.com>
2020-07-15 19:27:36 -0500

    Solver env problems

---
+++ lib/locator.dart
@@ -0,0 +1,13 @@
+/** 
+ * 
+import 'package:get_it/get_it.dart';
+import 'package:yt_todo/providers/task_provider.dart';
+import 'package:yt_todo/services/task_service.dart';
+
+GetIt locator = GetIt.instance;
+
+*/
+void setupLocator() {
+  //locator.registerLazySingleton(() => TaskProvider());
+  //locator.registerLazySingleton(() => TaskService());
+}
