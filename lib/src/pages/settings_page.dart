6cb7fafacfbfe31083adb662f5b1c9eccfea41c9
etenesaca <edgartenesaca@gmail.com>
2020-08-01 21:45:24 -0500

    Added list of locales

--- lib/src/pages/settings_page.dart
+++ lib/src/pages/settings_page.dart
@@ -16,6 +16,218 @@ class _SettingsPageState extends State<SettingsPage> {
   TextStyle textStyleItems;
   TextStyle textStyleUser;
 
+  List<String> localeList = [
+    'af-ZA',
+    'am-ET',
+    'ar-AE',
+    'ar-BH',
+    'ar-DZ',
+    'ar-EG',
+    'ar-IQ',
+    'ar-JO',
+    'ar-KW',
+    'ar-LB',
+    'ar-LY',
+    'ar-MA',
+    'arn-CL',
+    'ar-OM',
+    'ar-QA',
+    'ar-SA',
+    'ar-SY',
+    'ar-TN',
+    'ar-YE',
+    'as-IN',
+    'az-Cyrl-AZ',
+    'az-Latn-AZ',
+    'ba-RU',
+    'be-BY',
+    'bg-BG',
+    'bn-BD',
+    'bn-IN',
+    'bo-CN',
+    'br-FR',
+    'bs-Cyrl-BA',
+    'bs-Latn-BA',
+    'ca-ES',
+    'co-FR',
+    'cs-CZ',
+    'cy-GB',
+    'da-DK',
+    'de-AT',
+    'de-CH',
+    'de-DE',
+    'de-LI',
+    'de-LU',
+    'dsb-DE',
+    'dv-MV',
+    'el-GR',
+    'en-029',
+    'en-AU',
+    'en-BZ',
+    'en-CA',
+    'en-GB',
+    'en-IE',
+    'en-IN',
+    'en-JM',
+    'en-MY',
+    'en-NZ',
+    'en-PH',
+    'en-SG',
+    'en-TT',
+    'en-US',
+    'en-ZA',
+    'en-ZW',
+    'es-AR',
+    'es-BO',
+    'es-CL',
+    'es-CO',
+    'es-CR',
+    'es-DO',
+    'es-EC',
+    'es-ES',
+    'es-GT',
+    'es-HN',
+    'es-MX',
+    'es-NI',
+    'es-PA',
+    'es-PE',
+    'es-PR',
+    'es-PY',
+    'es-SV',
+    'es-US',
+    'es-UY',
+    'es-VE',
+    'et-EE',
+    'eu-ES',
+    'fa-IR',
+    'fi-FI',
+    'fil-PH',
+    'fo-FO',
+    'fr-BE',
+    'fr-CA',
+    'fr-CH',
+    'fr-FR',
+    'fr-LU',
+    'fr-MC',
+    'fy-NL',
+    'ga-IE',
+    'gd-GB',
+    'gl-ES',
+    'gsw-FR',
+    'gu-IN',
+    'ha-Latn-NG',
+    'he-IL',
+    'hi-IN',
+    'hr-BA',
+    'hr-HR',
+    'hsb-DE',
+    'hu-HU',
+    'hy-AM',
+    'id-ID',
+    'ig-NG',
+    'ii-CN',
+    'is-IS',
+    'it-CH',
+    'it-IT',
+    'iu-Cans-CA',
+    'iu-Latn-CA',
+    'ja-JP',
+    'ka-GE',
+    'kk-KZ',
+    'kl-GL',
+    'km-KH',
+    'kn-IN',
+    'kok-IN',
+    'ko-KR',
+    'ky-KG',
+    'lb-LU',
+    'lo-LA',
+    'lt-LT',
+    'lv-LV',
+    'mi-NZ',
+    'mk-MK',
+    'ml-IN',
+    'mn-MN',
+    'mn-Mong-CN',
+    'moh-CA',
+    'mr-IN',
+    'ms-BN',
+    'ms-MY',
+    'mt-MT',
+    'nb-NO',
+    'ne-NP',
+    'nl-BE',
+    'nl-NL',
+    'nn-NO',
+    'nso-ZA',
+    'oc-FR',
+    'or-IN',
+    'pa-IN',
+    'pl-PL',
+    'prs-AF',
+    'ps-AF',
+    'pt-BR',
+    'pt-PT',
+    'qut-GT',
+    'quz-BO',
+    'quz-EC',
+    'quz-PE',
+    'rm-CH',
+    'ro-RO',
+    'ru-RU',
+    'rw-RW',
+    'sah-RU',
+    'sa-IN',
+    'se-FI',
+    'se-NO',
+    'se-SE',
+    'si-LK',
+    'sk-SK',
+    'sl-SI',
+    'sma-NO',
+    'sma-SE',
+    'smj-NO',
+    'smj-SE',
+    'smn-FI',
+    'sms-FI',
+    'sq-AL',
+    'sr-Cyrl-BA',
+    'sr-Cyrl-CS',
+    'sr-Cyrl-ME',
+    'sr-Cyrl-RS',
+    'sr-Latn-BA',
+    'sr-Latn-CS',
+    'sr-Latn-ME',
+    'sr-Latn-RS',
+    'sv-FI',
+    'sv-SE',
+    'sw-KE',
+    'syr-SY',
+    'ta-IN',
+    'te-IN',
+    'tg-Cyrl-TJ',
+    'th-TH',
+    'tk-TM',
+    'tn-ZA',
+    'tr-TR',
+    'tt-RU',
+    'tzm-Latn-DZ',
+    'ug-CN',
+    'uk-UA',
+    'ur-PK',
+    'uz-Cyrl-UZ',
+    'uz-Latn-UZ',
+    'vi-VN',
+    'wo-SN',
+    'xh-ZA',
+    'yo-NG',
+    'zh-CN',
+    'zh-HK',
+    'zh-MO',
+    'zh-SG',
+    'zh-TW',
+    'zu-ZA'
+  ];
   @override
   Widget build(BuildContext context) {
     colorActions = Colors.teal;
@@ -199,15 +411,28 @@ class _SettingsPageState extends State<SettingsPage> {
     );
   }
 
-  buildItemLanguage(String title, String countryCode) {
-    final textStyle = TextStyle(fontSize: 14);
-    return DropdownMenuItem(
+  DropdownMenuItem<String> buildItemLanguage(String title, String countryCode) {
+    final textStyle = TextStyle(fontSize: 13);
+    return DropdownMenuItem<String>(
         value: countryCode,
         child: Row(
           children: <Widget>[
             Image.asset('assets/country_flags/$countryCode.png', width: 25),
             SizedBox(width: 5),
             Text(title, style: textStyle)
+          ],
+        ));
+  }
+
+  DropdownMenuItem<String> buildItemLocale(String locale) {
+    final textStyle = TextStyle(fontSize: 13);
+    return DropdownMenuItem<String>(
+        value: locale,
+        child: Row(
+          children: <Widget>[
+            Icon(Icons.language, color: Colors.grey, size: 25),
+            SizedBox(width: 5),
+            Text(locale, style: textStyle)
           ],
         ));
   }
@@ -220,13 +445,17 @@ class _SettingsPageState extends State<SettingsPage> {
       buildItemLanguage('Español', 'es'),
       buildItemLanguage('English', 'en'),
     ];
+
+    List<DropdownMenuItem<String>> appLocales =
+        localeList.map((e) => buildItemLocale(e)).toList();
+
     final textStyle = TextStyle(fontSize: 14, fontWeight: FontWeight.bold);
     showDialog(
       context: context,
       barrierDismissible: false,
       builder: (BuildContext context) {
         return StatefulBuilder(
-          builder: (BuildContext context, setState) {
+          builder: (BuildContext context, StateSetter setState) {
             return AlertDialog(
               title: Text('Idiomas'),
               content: Column(
@@ -235,6 +464,7 @@ class _SettingsPageState extends State<SettingsPage> {
                 children: <Widget>[
                   Text('Aplicación', style: textStyle),
                   DropdownButton(
+                      isExpanded: true,
                       value: _langApp,
                       items: appLanguages,
                       onChanged: (String val) {
@@ -243,8 +473,9 @@ class _SettingsPageState extends State<SettingsPage> {
                       }),
                   Text('Peliculas', style: textStyle),
                   DropdownButton(
+                      isExpanded: true,
                       value: _langMovies,
-                      items: appLanguages,
+                      items: appLocales,
                       onChanged: (String val) {
                         _langMovies = val;
                         setState(() {});
