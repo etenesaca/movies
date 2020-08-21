8ad51f1de38f148f7d1d329d74c3f9d2859290d3
etenesaca <edgartenesaca@gmail.com>
2020-08-20 20:33:22 -0500

    debug locale APP

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class MyApp extends StatelessWidget {
         LocaleNamesLocalizationsDelegate(),
       ],
       supportedLocales: S.delegate.supportedLocales,
-      locale: state.locale,
+      //locale: state.locale,
       debugShowCheckedModeBanner: false,
       title: 'Peliculas',
       routes: {
